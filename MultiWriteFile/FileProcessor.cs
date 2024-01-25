3916008a133f8f8155c16ad3b79503ef40d7a24a
Merin Gazell <Merin.Gazell@analog.com>
2024-01-25 11:53:54 -0500

    Add project files.

---
+++ MultiWriteFile/FileProcessor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MultiWriteFile
+{
+    internal class FileProcessor
+    {
+        public string FileName { get; set; }
+
+        private int _lines = 0;
+
+        public int Lines
+        {
+            get { return _lines; }
+        }
+
+        public FileProcessor(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath.Trim()))
+            {
+                throw new ArgumentNullException("filePath", "File path cannot be null or empty");
+            }
+
+            if(filePath.IndexOfAny(Path.GetInvalidPathChars()) != -1)
+            {
+                throw new ArgumentException("filePath", "File path contains invalid characters");
+            }
+
+            if(string.IsNullOrEmpty(Path.GetDirectoryName(filePath)) || string.IsNullOrEmpty(Path.GetFileName(filePath)))
+            {
+                throw new ArgumentException("filePath", "File path does not refer to a valid file path");
+            }
+
+            FileName = filePath;
+
+            InitFile();
+        }
+
+        public void WriteLIne(string text)
+        {
+            using (var streamWriter = new StreamWriter(FileName, true))
+            {
+                streamWriter.WriteLine(text);
+                streamWriter.Flush();
+            }
+            
+            //increment the lines counter if the write succeeds in a thread safe manner
+            Interlocked.Increment(ref _lines);
+        }
+
+        private void InitFile()
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(FileName));
+
+            //Create (replace if file exists) the file
+            File.Create(FileName).Close();
+        }
+    }
+}
---
+++ MultiWriteFile/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace MultiWriteFile
+{
+    internal class Program
+    {
+        private const string FILE_SUB_PATH = @"log/out.txt";
+
+        private static readonly object _fileLock = new();
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.Clear();
+
+                var fileProcessor = new FileProcessor(Path.Combine(Environment.CurrentDirectory, FILE_SUB_PATH));
+
+                //Write the initial record
+                fileProcessor.WriteLIne($"0, 0, {DateTime.Now:HH:mm:ss.fff}");
+                
+                var writeTasks = new Task[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    writeTasks[i] = Task.Run(() => WriteToFile(fileProcessor));
+                }
+
+                try
+                {
+                    await Task.WhenAll(writeTasks);
+                }
+                catch
+                {
+                    //UNwrap exceptions raised by tasks if any  
+                    var failedTasks = writeTasks.Where(t => t.IsFaulted).ToList();
+                    failedTasks.Select(t => t.Exception?.Flatten()).ToList().ForEach(ex =>
+                        {
+                            Console.WriteLine("An exception occured running a task: ");
+                            Console.WriteLine(ex.ToString());
+                        }
+                    );
+                }
+
+                Console.WriteLine($"Total lines {fileProcessor.Lines} written in {fileProcessor.FileName}");
+                
+                if(!Console.IsOutputRedirected)
+                {
+                    // Wait for key press to exit only if a terminal is attached
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An exception occured: ");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private static void WriteToFile(FileProcessor fileProcessor)
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+
+            for (int i = 0; i < 10; i++)
+            {
+                lock (_fileLock)
+                {
+                    fileProcessor.WriteLIne($"{fileProcessor.Lines}, {threadId}, {DateTime.Now:HH:mm:ss.fff}");
+                }
+            }
+        }
+    }
+}
